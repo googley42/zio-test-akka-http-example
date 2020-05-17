1e5cc2d9420628a3708b8d9db0d319cb3910a2a8
avinder.bahra <avinder.bahra@gmail.com>
2020-05-17 06:53:03 +0100

    dynamic creation of services from the environment working

--- src/test/scala/endpoint/ApiSpec2.scala
+++ src/test/scala/endpoint/ApiSpec2.scala
@@ -1,48 +1,57 @@
 package endpoint
 
+import akka.http.scaladsl.model.StatusCodes
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
 import endpoint.model.{Id, Model}
-import zio.logging.{Logger, Logging}
+import zio.clock.Clock
+import zio.console.Console
+import zio.logging.Logging
+import zio.test.Assertion._
 import zio.test._
 import zio.test.akkahttp.DefaultAkkaRunnableSpec
-import zio.test.akkahttp.RouteTestEnvironment.TestEnvironment
-import zio.{Has, Ref, Runtime, ZEnv, ZIO, ZLayer}
-import zio.logging.Logging
+import zio.{Ref, Runtime, ZIO, ZLayer}
+import zio.test.environment._
 
 object ApiSpec2 extends DefaultAkkaRunnableSpec {
+  import FailFastCirceSupport._
+  import io.circe.generic.auto._
 
   private val IdOne = Id("1")
-  private val IdTwo = Id("2")
-
-  import zio.test.environment._
-  val loggingLayer: ZLayer[zio.ZEnv, Nothing, Logging] = ((Live.default >>> TestConsole.debug) ++ (Live.default >>> TestClock.default)) >>> LoggingLive.testLayer
-  val yyy: ZLayer[Any, Nothing, Logging] = ZEnv.live >>> loggingLayer
-
-  private def layer(refMap: Ref[Map[Id, Model]]): ZLayer[Any, Nothing, Logging with Repository] =
-    yyy ++ (yyy >>> InMemoryRepository.inMemory(refMap))
-
-  private def layer2(
-    refMap: Ref[Map[Id, Model]],
-    logging: ZLayer[Any, Nothing, Logging]
-  ): ZLayer[Any, Nothing, Logging with Repository] =
-    logging ++ (logging >>> InMemoryRepository.inMemory(refMap))
-
-  val x: ZLayer[Logging, Nothing, Logging] = ZLayer.requires[Logging]
-  val y: ZLayer[Logging, Nothing, Logging] = ZLayer.identity[Logging]
-  val z: ZLayer[Logging, Nothing, Logging] = ZLayer.service[Logger[String]]
 
   override def spec =
     suite("ApiSpec")(
-      testM("") {
+      testM("get route returns entity and logs with correlation-id") {
         for {
-          _ <- Logging.info("XXX")
-          log <- ZIO.environment[Logging]
-          logLayer = ZLayer.succeed[Logger[String]](log.get)
+          refMap <- Ref.make[Map[Id, Model]](Map.empty + (IdOne -> Model(IdOne)))
+          apiLayer <- apiLayer(refMap)
+          api = new Api(Runtime.unsafeFromLayer(apiLayer))
+
+          assertRoute <- assertM(Get("/models/1") ~> api.routes)(
+            handled(
+              status(equalTo(StatusCodes.OK))
+                && entityAs[Model](isRight(equalTo(Model(IdOne))))
+            )
+          )
           vector <- TestConsole.output
-          r = new NoddyApi(Runtime.unsafeFromLayer(layer2(null, logLayer)))
-          _ <- ZIO.effect(println(s">${vector.head}<"))
-        } yield assertCompletes
-      }.provideCustomLayer(LoggingLive.testLayer)
+
+        } yield assertRoute && assert(vector.head)(
+          startsWithString(
+            "1970-01-01T00:00Z INFO  [correlation-id = 6c7dcaa9-e383-4993-be20-b8dd1949e19f] getting record Id(1)"
+          )
+        )
+      }
     )
+
+  private def apiLayer(refMap: Ref[Map[Id, Model]]) =
+    for {
+      clock <- ZIO.environment[Clock]
+      clockLayer = ZLayer.succeed[Clock.Service](clock.get)
+      console <- ZIO.environment[Console]
+      consoleLayer = ZLayer.succeed[Console.Service](console.get)
+      logLayer = consoleLayer ++ clockLayer >>> LoggingLive.testLayer
+      apiLayer = logLayer ++ (logLayer >>> InMemoryRepository.inMemory(refMap))
+    } yield apiLayer
+
 }
 
 class NoddyApi(r: Runtime[Logging with Repository])
